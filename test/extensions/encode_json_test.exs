847d7e366d1507fd0973dc1ffa92dd44f414ff2a
Ann-Kathrin Fochler <ak.falliano@hotmail.de>
2024-09-24 08:55:27 +0200

    Merged encode_json and encode_json_obj into one file, same for tests

--- test/extensions/encode_json_test.exs
+++ test/extensions/encode_json_test.exs
@@ -4,7 +4,8 @@ defmodule Extension.EncodeJsonTest do
   defmodule Logic do
     use JsonLogic.Base,
       extensions: [
-        JsonLogic.Extensions.EncodeJson
+        JsonLogic.Extensions.EncodeJson,
+        JsonLogic.Extensions.Obj
       ]
   end
 
@@ -54,5 +55,54 @@ defmodule Extension.EncodeJsonTest do
                  %{"sub_object" => %{"key1" => "foo"}}
                )
     end
+end
+
+
+describe "operation encode json obj" do
+
+  test "builds json obj from simple list" do
+    assert "{\"key1\":\"foo\"}" ==
+             Logic.apply(%{
+               "encode_json_obj" => [
+                 ["key1", "foo"]
+               ]
+             })
+  end
+  test "builds json object from nested list" do
+    assert "{\"key1\":\"foo\",\"key2\":42}" ==
+             Logic.apply(%{
+               "encode_json_obj" => [
+                 ["key1", "foo"],
+                 ["key2", 42],
+
+               ]
+             })
+
+  end
+  test "builds json object from double nested lists" do
+    assert "{\"key1\":\"foo\",\"key2\":42,\"key3\":\"bar\"}" ==
+             Logic.apply(%{
+               "encode_json_obj" => [
+
+                   ["key1", "foo"],
+                   ["key2", 42],
+                   ["key3", "bar"]
+
+               ]
+             })
+  end
+
+  test "builds json object from list that includes complete object resolved from data" do
+    assert "{\"key1\":\"foo\",\"key2\":\"bar\"}" ==
+             Logic.apply(
+               %{
+                 "encode_json_obj" => [
+                   %{"var" => "sub_object"},
+                   ["key2", "bar"]
+                 ]
+               },
+               %{"sub_object" => %{"key1" => "foo"}}
+             )
+  end
 end
 end

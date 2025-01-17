afad8ebe76dad6e75dea836ead353572eff99682
Menno Jager <menno@menno-labs.com>
2025-01-17 19:28:21 +0700

    handle mix.exs version

--- update-version.sh
+++ update-version.sh
@@ -19,7 +19,6 @@ sed -i "s/## \[$NEW_TAG_VERSION\] - $(date +%Y-%m-%d)/## [Unreleased]\n\n## \[$N
 
 # Update the link definitions in the CHANGELOG.md file
 # e.g. change `[unreleased]: https://github.com/box-id/json_logic_elixir/compare/<old_tag_version>...HEAD` to `[unreleased]: https://github.com/box-id/json_logic_elixir/compare/<new_tag_version>...HEAD`
-# old tag version is not known, so we need to find it in the file   
 OLD_TAG_VERSION=$(sed -n 's/\[unreleased\]: .*\/compare\/\(.*\)\.\.\.HEAD$/\1/p' CHANGELOG.md)
 
 sed -i "s/compare\/$OLD_TAG_VERSION\.\.\./compare\/$NEW_TAG_VERSION.../g" CHANGELOG.md
@@ -27,3 +26,7 @@ sed -i "s/compare\/$OLD_TAG_VERSION\.\.\./compare\/$NEW_TAG_VERSION.../g" CHANGE
 # Create new link definitions for the new tag version underneath the `[Unreleased]` link definition
 NEW_LINK="[$NEW_TAG_VERSION]: https:\/\/github.com\/box-id\/json_logic_elixir\/compare\/$OLD_TAG_VERSION...$NEW_TAG_VERSION"
 sed -i "/\[unreleased\]:.*/a $NEW_LINK" CHANGELOG.md
+
+# Update the version in the mix.exs file regardless of the old tag version
+sed -i "s/version: \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/version: \"$NEW_TAG_VERSION\"/" mix.exs
+

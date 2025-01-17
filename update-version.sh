4c5333f6cd6ce0b4dede05f7f85939919e605bfd
Menno Jager <menno@menno-labs.com>
2025-01-17 19:21:25 +0700

    changelog script done

--- update-version.sh
+++ update-version.sh
@@ -16,3 +16,14 @@ sed -i "s/## \[Unreleased\]/## \[$NEW_TAG_VERSION\] - $(date +%Y-%m-%d)/" CHANGE
 
 # Create a new `## [Unreleased]` section above the new version
 sed -i "s/## \[$NEW_TAG_VERSION\] - $(date +%Y-%m-%d)/## [Unreleased]\n\n## \[$NEW_TAG_VERSION\] - $(date +%Y-%m-%d)/" CHANGELOG.md
+
+# Update the link definitions in the CHANGELOG.md file
+# e.g. change `[unreleased]: https://github.com/box-id/json_logic_elixir/compare/<old_tag_version>...HEAD` to `[unreleased]: https://github.com/box-id/json_logic_elixir/compare/<new_tag_version>...HEAD`
+# old tag version is not known, so we need to find it in the file   
+OLD_TAG_VERSION=$(sed -n 's/\[unreleased\]: .*\/compare\/\(.*\)\.\.\.HEAD$/\1/p' CHANGELOG.md)
+
+sed -i "s/compare\/$OLD_TAG_VERSION\.\.\./compare\/$NEW_TAG_VERSION.../g" CHANGELOG.md
+
+# Create new link definitions for the new tag version underneath the `[Unreleased]` link definition
+NEW_LINK="[$NEW_TAG_VERSION]: https:\/\/github.com\/box-id\/json_logic_elixir\/compare\/$OLD_TAG_VERSION...$NEW_TAG_VERSION"
+sed -i "/\[unreleased\]:.*/a $NEW_LINK" CHANGELOG.md
